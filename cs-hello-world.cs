3d903c8fb9f76aa83ad29c3d0ad053d4df8b0fe6
Muhammed Karadas <muti5@windowslive.com>
2019-10-27 23:48:20 +0300

    cesitli-dillerde-hello-world

---
+++ cs-hello-world.cs
@@ -0,0 +1,12 @@
+using system;
+
+namespace cs-hello-world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World...\n");
+        }
+    }
+}
